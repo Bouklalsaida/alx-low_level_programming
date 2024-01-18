23c3e14e244ff13a2fbb834bf41f26cbafc90722
Bouklalsaida <Saidabouklal53@gmail.com>
2024-01-18 04:59:56 -0800

    q

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-int _putchar(char);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
